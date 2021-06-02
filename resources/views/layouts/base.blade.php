d3635c35f3026c0ab3383a218d6a2c08870db296
matsn0w <bartscholtus@gmail.com>
2021-06-02 19:18:02 +0200

    basic styling

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -15,8 +15,30 @@
 </head>
 <body>
     <section class="section">
-        <div id="app" class="container">
-            @yield('content')
+        <div class="container is-fluid">
+            <nav class="topnav section level mb-0 outside-border">
+                <div class="level-item">
+                    <img src="{{ asset('dist/img/dragon-small.png') }}" alt="logo">
+                    <span>{{ config('app.name') }}</span>
+                    <img src="{{ asset('dist/img/dragon-small-flipped.png') }}" alt="logo">
+                </div>
+
+                <div class="level-item shrink">
+                    <marquee class="mx-3" behavior="scroll" direction="left">
+                        Welkom bij {{ config('app.name') }}. Klik op deze tekst om de aanbiedingen van deze week te zien!
+                    </marquee>
+                </div>
+
+                <div class="level-item">
+                    <img src="{{ asset('dist/img/dragon-small.png') }}" alt="logo">
+                    <span>{{ config('app.name') }}</span>
+                    <img src="{{ asset('dist/img/dragon-small-flipped.png') }}" alt="logo">
+                </div>
+            </nav>
+
+            <div id="app" class="outside-border">
+                @yield('content')
+            </div>
         </div>
     </section>
 </body>
