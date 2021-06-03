bd59bef297acf535e116a26bdcc07f1c1f64b2c9
matsn0w <bartscholtus@gmail.com>
2021-06-03 17:04:30 +0200

    add authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NewsController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,16 @@ Route::view('/', 'pages.home')->name('home');
 Route::view('/contact', 'pages.contact')->name('contact');
 Route::view('/news', 'pages.news')->name('news');
 
-Route::name('admin.')->prefix('/admin')->group(function() {
+Route::name('auth.')->prefix('/auth')->group(function() {
+    Route::view('/login', 'auth.login')->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
+
+Route::name('admin.')->prefix('/admin')->middleware('auth')->group(function() {
+    Route::get('/', function () {
+        return 'admin';
+    })->name('index');
+
     Route::get('/news', [NewsController::class, 'admin'])->name('news.index');
     Route::get('/news/create', [NewsController::class, 'create'])->name('news.create');
     Route::get('/news/{news}/edit', [NewsController::class, 'edit'])->name('news.edit');
