53b341634dfbe820fb3726c4d2585b9e6ab1e7fb
matsn0w <bartscholtus@gmail.com>
2021-06-10 11:04:57 +0200

    add menu item model

---
+++ database/migrations/2021_06_10_104743_create_menu_items_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('number');
+            $table->foreignId('category_id')->constrained('menu_category');
+            $table->double('price');
+            $table->mediumText('description');
+            $table->timestamps();
+        });
+
+        Schema::create('menu_category', function(Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_category');
+        Schema::dropIfExists('menu_items');
+    }
+}
