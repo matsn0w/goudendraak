0a36f6f4de6deb94349e46a9e6d77a5e884aa3ce
matsn0w <bartscholtus@gmail.com>
2021-06-16 20:43:40 +0200

    remove userrequest from login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\UserRequest;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(UserRequest $request)
+    public function login(Request $request)
     {
-        $credentials = $request->validated();
+        $credentials = $request->validate([
+            'code' => ['required', 'integer'],
+            'password' => ['required'],
+        ]);
 
         if (Auth::attempt($credentials, false)) {
             $request->session()->regenerate();
