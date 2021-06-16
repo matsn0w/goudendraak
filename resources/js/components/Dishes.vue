e82c1fd055a3d66622e45adcc7d80b458cd503cd
matsn0w <bartscholtus@gmail.com>
2021-06-16 13:12:56 +0200

    add search

--- resources/js/components/Dishes.vue
+++ resources/js/components/Dishes.vue
@@ -1,4 +1,12 @@
 <template>
+    <form action="" method="get" class="block">
+        <div class="field">
+            <div class="control">
+                <input class="input" type="text" name="s" placeholder="Zoek een gerecht..." v-model="search">
+            </div>
+        </div>
+    </form>
+
     <table class="table is-striped is-hoverable">
         <thead>
             <tr>
@@ -11,7 +19,7 @@
         </thead>
 
         <tbody>
-            <tr v-for="item in items" :key="item.id">
+            <tr v-for="item in itemsSearched" :key="item.id">
                 <td v-html="item.name"></td>
                 <td>{{ item.number }}{{ item.number_addition }}</td>
                 <td>{{ euro(item.price) }}</td>
@@ -28,7 +36,30 @@ export default {
     data() {
         return {
             items: [],
+            search: '',
         }
+    },
+    computed: {
+        itemsSearched() {
+            const s = this.search?.toLowerCase();
+
+            return this.items.filter((item) => {
+                if (item.name?.toLowerCase().includes(s)) {
+                    return true;
+                }
+
+                if (item.category?.name.toLowerCase().includes(s)) {
+                    return true;
+                }
+
+                if (`${item.number}` == s
+                    || item.number_addition?.toLowerCase().includes(s)) {
+                    return true;
+                }
+
+                return false;
+            });
+        },
     },
     mounted() {
         axios.get('/api/v1/menuitems')
