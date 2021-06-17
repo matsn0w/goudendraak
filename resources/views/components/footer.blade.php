24f7910328ce29863ec7a1d88bbd2b2f9189eb56
matsn0w <bartscholtus@gmail.com>
2021-06-17 16:57:48 +0200

    fix styling

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,17 +1,27 @@
-<div class="has-text-centered mt-3">
-    <a href="{{ route('contact') }}" class="is-link">{{ __('messages.footer_to_contact') }}</a>
+<div class="has-text-centered is-flex is-flex-direction-column is-align-items-center mt-3">
+    <div class="block">
+        <a href="{{ route('contact') }}" class="is-link">{{ __('messages.footer_to_contact') }}</a>
+    </div>
 
     <form class="block" action="{{ route('lang.switch') }}" method="post">
         @csrf
 
         @php $locale = app()->getLocale();@endphp
 
-        <select name="lang" id="lang">
-            @foreach (config('languages') as $code => $name)
-                <option value="{{ $code }}"{{ $code == $locale ? ' selected' : '' }}>{{ $name }}</option>
-            @endforeach
-        </select>
+        <div class="field has-addons">
+            <div class="control">
+                <div class="select">
+                    <select name="lang" id="lang">
+                        @foreach (config('languages') as $code => $name)
+                            <option value="{{ $code }}"{{ $code == $locale ? ' selected' : '' }}>{{ $name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
 
-        <button type="submit">{{ __('messages.footer_change') }}</button>
+            <div class="control">
+                <button class="button" type="submit">{{ __('messages.footer_change') }}</button>
+            </div>
+        </div>
     </form>
 </div>
